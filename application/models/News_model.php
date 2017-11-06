fde0b0fd3bcdcb4bcefd1b98e8fd1efc2975ae84
shohan494 <shohan12224943@gmail.com>
2017-11-07 02:20:41 +0600

    working on post edit and created the update view and controller must have argument of the post id which will be edited, created also some part of model

--- application/models/News_model.php
+++ application/models/News_model.php
@@ -34,5 +34,22 @@ class News_model extends CI_Model {
 
 				    return $this->db->insert('news', $data);
 				}
+				
+				public function update_news()
+				{
+				    $this->load->helper('url');
+
+				    $slug = url_title($this->input->post('title'), 'dash', TRUE);
+
+				    $data = array(
+				        'title' => $this->input->post('title'),
+				        'slug' => $slug,
+				        'text' => $this->input->post('text'),
+				        'news_user_id' => $this->ion_auth->user()->row()->id
+				    );
+
+				    return $this->db->insert('news', $data);
+				}
+
 }
 
